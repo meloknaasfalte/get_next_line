1ab3771ebae61953b1bb6d20f26fcb01aa838352
Vladislav Musin <vladislavmusin@MacBook-Air-Vladislav.local>
2021-06-03 15:13:56 +0300

    gnl

---
+++ get_next_line.c
@@ -0,0 +1,86 @@
+#include "get_next_line.h"
+
+char	*ft_free(char **r, char *buf)
+{
+	char *tmp;
+
+	tmp = *r;
+	*r = ft_strjoin(*r, buf);
+	free(tmp);
+	return (*r);
+}
+char *ft_get_line(char **r, char **line, int *flag)
+{
+	char	*n;
+
+	if (!(*flag))
+		*flag = 0;
+	n = ft_strchr(*r, '\n');
+	if (*flag == 1)
+		return (*line) ;
+	if (n == 0)
+	{
+		*line = ft_strdup(*r);
+		*r = NULL;
+		*flag = 1;
+		return (*line);
+	}
+	*n = '\0';
+	*line = ft_strdup(*r);
+	*r = n + 1;
+	return (*line);
+}
+
+int	ft_free_end(char **begin)
+{
+	free(*begin);
+	return (0);
+}
+
+int 	get_next_line(int fd, char **line)
+{
+	char		buf[BUFF_SIZE + 1];
+	int 		result;
+	char static	*remainder;
+	static int 	flag;
+	static char *begin;
+
+	result = 1;
+	if (!remainder)
+		remainder = ft_strdup("");
+	while (result)
+	{
+		result = read(fd, buf, BUFF_SIZE);
+		if (result == -1)
+			return (-1);
+		if (result == 0)
+			break;
+		buf[result] = '\0';
+		remainder = ft_free(&remainder, buf);
+	}
+	if (!begin)
+		begin = remainder;
+	*line = ft_get_line(&remainder, line, &flag);
+	if (!remainder)
+		return (ft_free_end(&begin));
+	return (1);
+}
+
+int	main()
+{
+	char	*line;
+	int		fd;
+	int		res;
+
+	line = NULL;
+	fd = open("text_try.txt", O_RDONLY);
+	while ((res = get_next_line(fd, &line)))
+	{
+		printf("%s : %i\n", line, res);
+		free(line);
+	}
+	get_next_line(fd, &line);
+	printf("%s : %i\n", line, res);
+	free(line);
+	return (0);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,116 @@
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	l;
+	size_t	len1;
+	size_t	len2;
+	char	*r;
+
+	l = 0;
+	i = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	r = malloc(sizeof(char) *(len1 + len2 + 1));
+	if (!r)
+		return (NULL);
+	while (s1[i])
+	{
+		r[i] = s1[i];
+		i++;
+	}
+	while (s2[l])
+		r[i++] = s2[l++];
+	r[i] = '\0';
+	return (r);
+}
+
+char	*ft_strchr (const char *str, int ch)
+{
+	size_t	i;
+	char	c;
+
+	i = 0;
+	c = (char)ch;
+	while (str[i] != '\0' && str[i] != c)
+		i++;
+	if (str[i] == c)
+		return ((char *)(str + i));
+	return (NULL);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*p;
+	size_t	r;
+
+	r = size * nmemb;
+	p = malloc(r);
+	if (!p)
+		return (NULL);
+	if (p)
+		ft_memset(p, '\0', r);
+	return (p);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	size_t	l;
+	char	*ds ;
+
+	l = 0;
+	i = ft_strlen(s);
+	ds = malloc(i + 1);
+	if (!ds)
+		return (NULL);
+	while (s[l])
+	{
+		ds[l] = s[l];
+		l++;
+	}
+	ds[l] = '\0';
+	return (ds);
+}
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*p;
+
+	i = 0;
+	p = (char *) s;
+	while (i < n)
+	{
+		p[i] = (char) c;
+		i++;
+	}
+	return (s);
+}
+
+char	*ft_strcpy (char *dst, const char *src)
+{
+	int 	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
